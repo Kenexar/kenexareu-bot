84a2f7805753ac7b2dc7ecb1544dd1207280304a
exersalza <squidodle44@gmail.com>
2021-12-30 17:54:32 +0100

    f

--- src/cogs/casino.py
+++ src/cogs/casino.py
@@ -5,10 +5,10 @@ class Casino(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
     @commands.Command
     async def blackjack(self, ctx):
         pass
+
 
 def setup(bot):
     bot.add_cog(Casino(bot))
