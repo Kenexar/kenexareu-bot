c7f4427b567d96bc99b79f4ca5dd485ce3b0894b
exersalza <squidodle44@gmail.com>
2022-01-04 01:22:54 +0100

    aklöjfdskaljhflökasjf

--- src/CMDstart.sh
+++ src/CMDstart.sh
@@ -5,9 +5,4 @@ screen -U -m -d -S sunside-bot python3.8 __main__.py
 
 echo "Bot is started"
 
-screen -X -S git-updater-sunside kill
-screen -U -m -d -S git-updater-sunside python3.8 git-updater.py
-
-echo "Git-Updater started"
-
 eval "screen -r kenexardcbot"
--- src/start.sh
+++ src/start.sh
@@ -5,8 +5,3 @@ screen -U -m -d -S sunside-bot python3.8 __main__.py
 
 echo "Bot is started"
 
-screen -X -S gitUpdaterSunside kill
-screen -U -m -d -S gitUpdaterSunside python3.8 git-updater.py
-
-echo "Git-Updater started"
-
