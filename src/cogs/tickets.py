ecbd7c24424bdd01e1862b74da3bc8bb3589aa6f
exersalza <squidodle44@gmail.com>
2022-01-08 07:55:58 +0100

    f

--- src/cogs/tickets.py
+++ src/cogs/tickets.py
@@ -97,6 +97,7 @@ class Ticket(commands.Cog):
         createticket = await create_ticket()
 
         await channel.send(embed=createticket[0], view=createticket[1])
+        #git commiting makes fun
 
     @commands.Cog.listener()
     async def on_interaction(self, interaction):
