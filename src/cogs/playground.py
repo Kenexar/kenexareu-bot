60a86008463c67d5601d4e7a0e810584cc6cb49d
exersalza <squidodle44@gmail.com>
2022-01-20 09:51:13 +0100

    testing some stuff

--- src/cogs/playground.py
+++ src/cogs/playground.py
@@ -1,5 +1,27 @@
 import fileinput
+from slpp import slpp as lua
 
-with fileinput.FileInput('etc/config_preset', inplace=True) as file:
-    for line in file:
-        print(line.replace('config', 'esx_mg13'), end='')
+data = {'Shared': [{'model': 'buccaneer2', 'label': 'Gang Fahrzeug', 'price': 8000}], 'boss': [{'model': 'jugular', 'label': 'Boss Fahrzeug', 'price': 8000}]}
+
+print(lua.encode(data), end='')
+print(lua.decode("""Config.AuthorizedVehicles = {
+	["Shared"] = {
+		{
+		["model"] = "buccaneer2",
+		["label"] = "Gang Fahrzeug",
+		["price"] = 8000
+	}
+	},
+	["boss"] = {
+			{
+			["model"] = "jugular",
+			["label"] = "Boss Fahrzeug",
+			["price"] = 8000
+		}
+	}}"""), end='')
+
+# str_ = ''
+#
+# with fileinput.FileInput('etc/config_preset', inplace=True) as file:
+#     for line in file:
+#         print(line.replace('config', str_), end='')
