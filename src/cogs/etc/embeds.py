975ca5ef8c3c35e5bfe8964d2b8344626b2bb5d8
exersalza <squidodle44@gmail.com>
2022-01-24 18:36:04 +0100

    WIR TANZEN IM VIERECK, WIR TANZEN KONZENTRIERT

--- src/cogs/etc/embeds.py
+++ src/cogs/etc/embeds.py
@@ -13,5 +13,8 @@ def help_embed(mode='normal'):
         embed.add_field(name=f'{PREFIX}listmodules', value='Des: List all current modules in Cogs', inline=False)
         embed.add_field(name=f'{PREFIX}reload (module name)', value=f'Des: Reload giving Cog module\nExample: {PREFIX}reload cogs.casino', inline=False)
 
+    if mode == 'qr-creator':
+        pass
+
     return embed
 
--- src/playground.py
+++
@@ -1,5 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='Process')
-parser.add_argument('colores', metavar='N', type=int, nargs='-', help='coggers')
-print(parser.parse_args(['--']))
