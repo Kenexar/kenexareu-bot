3062c73b38609ad846641f29a3122ce8f826224e
exersalza <squidodle44@gmail.com>
2022-01-24 18:43:23 +0100

    git updater implement

---
+++ src/git-updater.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+import time
+from datetime import datetime
+
+
+def update_check(result=subprocess.run(['git', 'pull'], stdout=subprocess.PIPE, check=True)):
+    return result.stdout.decode('utf-8')
+
+
+rounds = 0
+runs = 0
+
+if __name__ == '__main__':
+    while True:
+        runs += 1
+        if datetime.now().strftime('%H') in ['12', '0']:
+            if 'already up to date.' in update_check().lower():
+                continue
+
+            time.sleep(120)
+            os.system('./restart.sh')
+            rounds += 1
+
+        time.sleep(3600)
--- src/restart.sh
+++ src/restart.sh
@@ -1,4 +1,4 @@
-eval "source /home/sunside-bot/venv/bin/activate"
+eval "source /home/kenexareu-bot/venv/bin/activate"
 
-screen -X -S sunside-bot kill
-screen -U -m -d -S sunside-bot python3.8 __main__.py
+screen -X -S kenexareu-bot kill
+screen -U -m -d -S kenexareu-bot python3.8 __main__.py
