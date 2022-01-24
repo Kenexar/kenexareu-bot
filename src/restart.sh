1d155e90aa217f2f550f092fa7eab46aafbab9f3
exersalza <squidodle44@gmail.com>
2022-01-24 18:55:26 +0100

    falsche schlange überall

--- src/restart.sh
+++ src/restart.sh
@@ -1,4 +1,18 @@
 eval "source /home/kenexareu-bot/venv/bin/activate"
 
+if [ $# -eq 0 ]; then
 screen -X -S kenexareu-bot kill
 screen -U -m -d -S kenexareu-bot python3.8 __main__.py
+echo "Bot is started"
+exit 1
+fi
+
+if [ $1 == "-r" ]; then
+screen -X -S kenexareu-bot kill
+screen -U -m -d -S kenexareu-bot python3.8 __main__.py
+echo "Bot is started -r"
+exit 1
+fi
+
+
+
