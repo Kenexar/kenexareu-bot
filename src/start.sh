45e5c8ccee52339f729eb7fb7d304e7031e59195
exersalza <squidodle44@gmail.com>
2022-01-24 18:54:02 +0100

    git updater implement

--- src/start.sh
+++ src/start.sh
@@ -1,7 +1,18 @@
-eval "source /home/sunside-bot/venv/bin/activate"
+eval "source /home/kenexareu-bot/venv/bin/activate"
 
-screen -X -S sunside-bot kill
-screen -U -m -d -S sunside-bot python3.8 __main__.py
-
+if [ $# -eq 0 ]; then
+screen -X -S kenexareu-bot kill
+screen -U -m -d -S kenexareu-bot python3.8 __main__.py
 echo "Bot is started"
+exit 1
+fi
+
+if [ $1 == "-r" ]; then
+screen -X -S kenexareu-bot kill
+screen -U -m -d -S kenexareu-bot python3.8 __main__.py
+echo "Bot is started -r"
+exit 1
+fi
+
+
 
